f913e44264dadd1f176b07ecc6b0a873a957cb4c
hyunju77 <guswn0280@naver.com>
2020-05-17 18:06:30 +0900

    Contoller 내용 추가 - 상세보기를 위한 새로운 RequestMapping

--- web/BoardController.java
+++ web/BoardController.java
@@ -15,4 +15,26 @@ public class BoardController {
 
         return "board/boardList.tiles";
     }
+
+    @RequestMapping (value="/boardDetail.do")
+    public String boardDetail (HttpServletRequest request, ModelMap model) throws Exception {
+        String boardno = request.getParameter("boardno");
+        //getParameter로 jsp 화면에서 만든 boardno를 가져와 String 형태의 boardno에 저장
+        int boardnoInt = Integer.parseInt(boardno);
+        //String 형태로 가져오기 때문에 Int 형으로
+        BoardVO boardVO = new BoardVO();
+        try {
+            boardVO = boardService.detailBoard(boardnoInt);
+            //DB에서 값들을 가져와 boardVO에 저장
+        } catch (Exception e) {
+            e.printStackTrace();
+    }
+    model.addAttribute("boardVO", boardVO);
+    return "board/boardDetail.tiles";
 }
+
+}
+
+
+
+
