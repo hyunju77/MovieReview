fde3f516c1a329aef386aaf2c3daffb1e404ff8b
Clustern <homerun372@gmail.com>
2020-07-01 16:58:06 +0900

    원 리포지트와 동기화

--- html/js/template.js
+++ html/js/template.js
@@ -1,5 +1,5 @@
 module.exports = {
-  Post_Reader_HTML:function(title, post_data, comment_data) {
+  Post_Reader_HTML:function(title, post_data, comment_data, back, modify, remove) {
     if(!post_data.modifydate){
       var create_modify_date = `<h5>게시일 : ${post_data.createdate}</h5>`;
     } else {
@@ -8,124 +8,95 @@ module.exports = {
     return `
       <!DOCTYPE html>
       <html>
-          <head>
-              <meta charset="utf-8">
+        <head>
+          <meta charset="utf-8">
               <title>${title}</title>
               <script src="js/jquery-3.5.1.min.js"></script>
-              <link rel="stylesheet" href="css/common.css">
+              <link rel="stylesheet" href="css/commen.css">
+              <link rel="stylesheet" href="css/post_read.css">
           </head>
-          <body>
-              <table id="table_body" >
-                  <tr id="header">
-                      <td colspan="3">
-                          웹페이지 타이틀 + 로고
-                      </td>
-                  </tr>
-                  <tr>
-                      <td colspan="3" class="top_menu">
-                        <div class="h-container">
-                            <div class="item middle">
-                              <form action="/" method="get">
-                                <input type="submit" value="돌아가기" href="/">
-                              </form>
-                            </div>
-                            <div class="item middle">
-                              <form action="/post_update" method="post">
-                                <input type="hidden" name="id" value="${post_data.post_id}">
-                                <input type="submit" value="변경">
-                              </form>
-                            </div>
-                            <div class="item middle">
-                              <form action="/post_delete_process" method="post" onsubmit="return recheck(1)">
-                                <input type="hidden" name="id" value="${post_data.post_id}">
-                                <input type="submit" value="삭제">
-                              </form>
-                            </div>
-                            <div class="item last">login</div>
-                            </div>
-                        </div>    
-                      </td>
-                  </tr>
-                  <tr>
-                      <td id="navigation" class="sideline">
-                          <ul>
-                              <strong>장르별</strong>
-                              <li><a href="">드라마</a></li>
-                              <li><a href="">액션</a></li>
-                          </ul>
-                          <ul>
-                              <strong>연도별</strong>
-                              <li><a href="">(1990~2000)</a></li>
-                              <li><a href="">(2000~2010)</a></li>
-                              <li><a href="">(2010~2020)</a></li>
-                          </ul>
-                      </td>
-                      <td id="middle_side" class="sideline">
-                          <h3>${post_data.title}</h3>
-                          ${create_modify_date}
-                          <hr size="1" color="grey">
-                          ${post_data.description}
-                          <br><br><br>
-                          <hr size="10" color="grey">
-                          <br><br><br>
-                          <div>
-                            <form name="comment_creater" action="/comment_create_process" method="post" onsubmit="return comment_data_intergrity(creater)">
-                              <input type="hidden" name="post_id" value="${post_data.post_id}">
-                              <input type="hidden" name="user_id" value="1">
-                              <input type="hidden" name="score" value="5">
-                              <input type="text" name="description" placeholder="댓글 입력">
-                              <table>
-                                <tr>
-                                  <td> 평점 : </td>
-                                  <td>
-                                    <div class="starRev">
-                                      <span class="starR1 on">별1_왼쪽</span>
-                                      <span class="starR2 on">별1_오른쪽</span>
-                                      <span class="starR1 on">별2_왼쪽</span>
-                                      <span class="starR2 on">별2_오른쪽</span>
-                                      <span class="starR1 on">별3_왼쪽</span>
-                                      <span class="starR2">별3_오른쪽</span>
-                                      <span class="starR1">별4_왼쪽</span>
-                                      <span class="starR2">별4_오른쪽</span>
-                                      <span class="starR1">별5_왼쪽</span>
-                                      <span class="starR2">별5_오른쪽</span>
-                                    </div>
-                                  </td>
-                              </table>
-                              <input type="submit" value="등록">
-                            </form>
-                            <table border = 1 class="comments">
-                              <tr>
-                                <th>닉네임</th>
-                                <th>댓글내용</th>
-                                <th>평점</th>
-                                <th>작성일</th>
-                                <th>수정</th>
-                                <th>삭제</th>
-                              </tr>
-                              ${comment_data}
-                            </table>
+          <body id="main">
+              <div id="myMenu">
+                  <div class="h-container">
+                    <div class="item middle">
+                      <form action="/" method="get">
+                        <input type="submit" value="돌아가기" href="/">
+                        </form>
+                      </div>
+                      <div class="item middle">
+                        <form action="/post_update" method="post">
+                          <input type="hidden" name="id" value="${post_data.post_id}">
+                          <input type="submit" value="변경">
+                        </form>
+                      </div>
+                      <div class="item middle">
+                        <form action="/post_delete_process" method="post" onsubmit="return recheck(1)">
+                          <input type="hidden" name="id" value="${post_data.post_id}">
+                          <input type="submit" value="삭제">
+                        </form>
+                      </div>
+                      <div class="item last">login</div>
+                  </div>
+              </div>
+                <nav id="myNav">
+                    navigation
+                    <ul>장르
+                        <a href=""><li>장르1</li></a>
+                        <a href=""><li>장르2</li></a>
+                    </ul>
+                    <ul>연도
+                        <a href=""><li>(2000~2010)</li></a>
+                        <a href=""><li>(2010~2020)</li></a>
+                    </ul>
+                </nav>
+                <section id="post_Section">
+                  <h3>${post_data.title}</h3>
+                  ${create_modify_date}
+                  ${post_data.description}
+                </section>
+                <div>
+                  <form name="comment_creater" action="/comment_create_process" method="post" onsubmit="return comment_data_intergrity(creater)">
+                    <input type="hidden" name="post_id" value="${post_data.post_id}">
+                    <input type="hidden" name="user_id" value="1">
+                    <input type="hidden" name="score" value="2.5">
+                    <input type="text" name="description" placeholder="댓글 입력">
+                    <table>
+                      <tr>
+                        <td> 평점 : </td>
+                        <td>
+                          <div class="starRev">
+                            <span class="starR1 on">별1_왼쪽</span>
+                            <span class="starR2 on">별1_오른쪽</span>
+                            <span class="starR1 on">별2_왼쪽</span>
+                            <span class="starR2 on">별2_오른쪽</span>
+                            <span class="starR1 on">별3_왼쪽</span>
+                            <span class="starR2">별3_오른쪽</span>
+                            <span class="starR1">별4_왼쪽</span>
+                            <span class="starR2">별4_오른쪽</span>
+                            <span class="starR1">별5_왼쪽</span>
+                            <span class="starR2">별5_오른쪽</span>
                           </div>
-                      </td>
-                      <td id="right_side" class="sideline">
-                          <div class="movie">
-                              <h2 id="right_side_title">영화목록</h2>
-                              <ul>
-                                  <li data-url="move1.html">라스트 데이스 아오브아</li>
-                                  <li data-url="move2.html">영화이름2(개봉년도)</li>
-                              </ul>
-                          <div id="left_right_button">
-                              <button class="go_left"> << </button>
-                              &emsp;&ensp;6/1&ensp;&emsp;
-                              <button class="go_right"> >> </button>
-                          </div>
-                      </td>
-                  </tr>
-              </table>
+                        </td>
+                    </table>
+                    <input type="submit" value="등록">
+                  </form>
+                  <table border = 1 id="comments">
+                    <tr>
+                      <th>닉네임</th>
+                      <th>댓글내용</th>
+                      <th>평점</th>
+                      <th>작성일</th>
+                      <th>수정</th>
+                      <th>삭제</th>
+                    </tr>
+                    ${comment_data}
+                  </table>
+                </div>
+              </nav>
               <script src="js/html_functions.js"></script>
           </body>
       </html>
-    `
+    `;
   },
 
   Editer_HTML:function(movies, post_data) {
@@ -158,7 +129,7 @@ module.exports = {
       <head>
         <meta charset="utf-8">
         <title>게시물 작성</title>
-        <link rel="stylesheet" href="css/common.css">
+        <link rel="stylesheet" href="css/commen.css">
       </head>
       <body>
         <div id="myMenu">
